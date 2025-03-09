404e55bcadc36879cb97aebef5322bd58e9cfdd7
Chris <chrismc16@outlook.com>
2025-03-08 19:26:18 -0800

    - Updated Header.jsx; Now updates based on if the user is logged in or not.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import styles from '../css/header.module.css'
 import { useEffect, useState, useRef, useContext } from "react";
 import CartItem from "./CartItem";
 import { CartContext } from "../services/CartContext";
+import { useAuth } from "../services/AuthContext"
 
 const Header = () => {
     const { cart } = useContext(CartContext);
+    const { user, logout } = useAuth();
     const [isCartOpen, setIsCartOpen] = useState(false);
     const cartRef = useRef(null);
 
@@ -32,42 +34,48 @@ const Header = () => {
                     <a href="/contact">Contact Us</a>
                 </nav>
             </div>
-            
-            <div className={styles.header_right}>
-                <div className={styles.cart_container}>
-                    <button className={styles.cart_button} onClick={toggleCart}>
-                        <IoCartOutline className={styles.icon} />
-                        <a className={styles.cart_text} href="#">Cart ({cart.reduce((sum, item) => sum + item.quantity, 0)})</a>
-                    </button>
-                    
-                    {isCartOpen && (
-                        <div className={styles.cart_dropdown}>
-                            <div className={styles.cart_header}>
-                                <h3 className={styles.cart_title}>My Cart ({cart.reduce((sum, item) => sum + item.quantity, 0)})</h3>
-                                <button className={styles.button_close_cart} onClick={() => setIsCartOpen(false)}>
-                                    <IoClose className={styles.icon_close_cart}/>
-                                </button>
-                            </div>
-                            {cart.length > 0 ? (
-                                <ul className={styles.list_cart}>
-                                    {cart.map((item, index) => (
-                                        <li key={index}>
-                                            <CartItem cartItem={item} />
-                                        </li>
-                                    ))}
-                                </ul>
-                            ) : (
-                                <div className={styles.cart_empty_container}>
-                                    <p className={styles.empty_cart_text}>Your cart is empty.</p>
+
+            {user ? (
+                <div className={styles.header_right}>
+                    <div className={styles.cart_container}>
+                        <button className={styles.cart_button} onClick={toggleCart}>
+                            <IoCartOutline className={styles.icon} />
+                            <a className={styles.cart_text} href="#">Cart ({cart.reduce((sum, item) => sum + item.quantity, 0)})</a>
+                        </button>
+                        
+                        {isCartOpen && (
+                            <div className={styles.cart_dropdown}>
+                                <div className={styles.cart_header}>
+                                    <h3 className={styles.cart_title}>My Cart ({cart.reduce((sum, item) => sum + item.quantity, 0)})</h3>
+                                    <button className={styles.button_close_cart} onClick={() => setIsCartOpen(false)}>
+                                        <IoClose className={styles.icon_close_cart}/>
+                                    </button>
                                 </div>
-                            )}
-                            {cart.length > 0 && <button className={styles.button_checkout}>Checkout (${(cart.reduce((sum, item) => sum + item.quantity * item.price, 0)).toFixed(2)})</button>}
-                        </div>
-                    )}
+                                {cart.length > 0 ? (
+                                    <ul className={styles.list_cart}>
+                                        {cart.map((item, index) => (
+                                            <li key={index}>
+                                                <CartItem cartItem={item} />
+                                            </li>
+                                        ))}
+                                    </ul>
+                                ) : (
+                                    <div className={styles.cart_empty_container}>
+                                        <p className={styles.empty_cart_text}>Your cart is empty.</p>
+                                    </div>
+                                )}
+                                {cart.length > 0 && <button className={styles.button_checkout}>Checkout (${(cart.reduce((sum, item) => sum + item.quantity * item.price, 0)).toFixed(2)})</button>}
+                            </div>
+                        )}
+                    </div>
+                    
+                    <a href="" className={styles.sign_in} onClick={logout}>Account</a>
                 </div>
-                
+            ) : (
                 <a href="/login" className={styles.sign_in}>Sign In</a>
-            </div>
+            )}
+            
+            
 
             {isCartOpen && <div className={styles.overlay} onClick={() => setIsCartOpen(false)}></div>}
         </header>
