8480353113fb9bbc1ac589329fc9099a47395127
Chris <chrismc16@outlook.com>
2025-03-09 03:53:36 -0700

    - Updated Login.jsx; Updated to work with new Login API endpoint and use the notification system.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import '../css/page-login.css'
 import { useState } from "react";
 import { useAuth } from "../services/AuthContext"
+import { useNotification } from "../components/Notification"
 
 export default function Login() {
+    const { showNotification } = useNotification();
     const { login } = useAuth();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -14,7 +16,7 @@ export default function Login() {
         fetch("https://findthefrontier.ca/frontier_books/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ username: "", email, password }),
+            body: JSON.stringify({ user_id: 0, user_name: "test", user_email: email, user_password: password }),
         })
             .then((response) => response.json())
             .then((data) => {
@@ -23,13 +25,16 @@ export default function Login() {
                     login(data.access_token);
                     location.href = '/';
                     setError(null);
+                    showNotification("Login Successful!")
                 } else {
                     setError("Login Failed: Invalid Credentials");
+                    showNotification("Login Failed: Invalid Credentials")
                 }
             })
             .catch((error) => {
                 setError("Login Failed: " + error.message);
                 console.error("Login Failed: " + error.message);
+                showNotification("An unknown error occured.")
             });
     }
 
