6d6c8e49fb3c275adfaa6bb674ec056cf12481c4
Chris <chrismc16@outlook.com>
2025-03-18 20:20:52 -0700

    - Updated CartItem.jsx; Now uses global ref to notify the app when the cart has changed.

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -3,8 +3,8 @@ import { IoAdd, IoRemove, IoTrashOutline } from "react-icons/io5";
 import { CartContext } from "../services/CartContext";
 import styles from '../css/CartItem.module.css'
 
-export default function CartItem({ cartItem, hasCartUpdatedRef }) {
-    const { updateQuantity, removeItem } = useContext(CartContext);
+export default function CartItem({ cartItem }) {
+    const { isCartSaved, updateQuantity, removeItem } = useContext(CartContext);
 
     return (
         <div className={styles.item_card}>
@@ -21,10 +21,10 @@ export default function CartItem({ cartItem, hasCartUpdatedRef }) {
                 
                 <div className={styles.quantity_pricing}>
                     <div className={styles.quantity_controls}>
-                        <button className={styles.button_change_quantity} onClick={() => {updateQuantity(cartItem.title, cartItem.quantity - 1); hasCartUpdatedRef.current = true;}}><IoRemove className={styles.icon_change_quantity}/></button>
+                        <button className={styles.button_change_quantity} onClick={() => {updateQuantity(cartItem.title, cartItem.quantity - 1); isCartSaved.current = false;}}><IoRemove className={styles.icon_change_quantity}/></button>
                         <input type="text" className={styles.item_quantity} value={cartItem.quantity} min="1" disabled></input>
-                        <button className={styles.button_change_quantity} onClick={() => {updateQuantity(cartItem.title, cartItem.quantity + 1); hasCartUpdatedRef.current = true;}}><IoAdd className={styles.icon_change_quantity}/></button>
-                        <button className={styles.remove_button} onClick={() => {removeItem(cartItem.title); hasCartUpdatedRef.current = true;}}><IoTrashOutline /></button>
+                        <button className={styles.button_change_quantity} onClick={() => {updateQuantity(cartItem.title, cartItem.quantity + 1); isCartSaved.current = false;}}><IoAdd className={styles.icon_change_quantity}/></button>
+                        <button className={styles.remove_button} onClick={() => {removeItem(cartItem.title); isCartSaved.current = false;}}><IoTrashOutline /></button>
                     </div>
                     <p className={styles.item_price}>${(cartItem.price * cartItem.quantity).toFixed(2)}</p>
                 </div>
