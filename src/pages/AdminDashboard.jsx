cc6874498ce5e826bc72384063d164818da6fe64
Chris <chrismc16@outlook.com>
2025-03-23 23:24:32 -0700

    - Updated AdminDashboard.jsx; Now retrieves and displays users

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,47 +1,108 @@
 import styles from '../css/PageAdminDashboard.module.css'
-import { IoChevronBackOutline } from "react-icons/io5";
-import CheckoutItem from "../components/CheckoutItem";
+import { IoChevronBackOutline, IoReload, IoSaveOutline } from "react-icons/io5";
 
-import { useContext, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import { CartContext } from "../services/CartContext";
+import { useAuth } from '../services/AuthContext';
+import { useNotification } from '../components/Notification';
 
 export default function AdminDashboard() {
-    const [selectedSection, setSelectedSection] = useState("inventory");
+    const [selectedTable, setSelectedTable] = useState("inventory");
+    const [tableData, setTableData] = useState({});
+
+    const { showNotification } = useNotification();
+    const { getValidAccessToken } = useAuth();
+
+    const tableColumnHeaders = ({
+        "inventory": ["Item ID", "Authour", "Title", "Description", "Price"],
+        "users": ["User ID", "Email", "Username", "Role"],
+        "orders": ["Order ID", " User ID", "Items", "Cost", "Status"],
+    });
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!tableData[selectedTable]) {
+            fetchData(selectedTable);
+        }
+    }, [selectedTable]);
+
+    const fetchData = async (section) => {
+        try {
+            const response = await fetch("https://findthefrontier.ca/frontier_books/users", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${getValidAccessToken()}`
+                },
+            });
+            if (!response.ok) throw new Error("Error Getting Users");
+            const result = await response.json();
+
+            setTableData((prevData) => ({
+                ...prevData,
+                [selectedTable]: result.users
+            }));
+            console.log(tableData);
+            showNotification("Loaded Data");
+        } catch (err) {
+            console.log("Err: ", err.message);
+            showNotification("Error");
+        }
+    };
 
     return (
         <div className={styles.pageRootLayout}>
             <div className={styles.rowLayout}>
                 <div className={styles.navigationLayout}>
                     <div className={styles.navigationHeader}>
-                        <button className={styles.buttonClose} onClick={() => {navigate('/')}}>
+                        <button className={styles.actionButton} onClick={() => {navigate('/')}}>
                             <IoChevronBackOutline className={styles.iconClose} />
                         </button>
                         <h2 className={styles.navigationText}>Back</h2>
                     </div>
                     <ul>
-                        <li>
-                            <button className={styles.buttonNavigation} onClick={() => {setSelectedSection("inventory")}}>Inventory</button>
-                        </li>
-                        <li>
-                            <button className={styles.buttonNavigation} onClick={() => {setSelectedSection("users")}}>Users</button>
-                        </li>
-                        <li>
-                            <button className={styles.buttonNavigation} onClick={() => {setSelectedSection("orders")}}>Orders</button>
-                        </li>
+                        <li><button className={styles.buttonNavigation} onClick={() => {setSelectedTable("inventory")}}>Inventory</button></li>
+                        <li><button className={styles.buttonNavigation} onClick={() => {setSelectedTable("users")}}>Users</button></li>
+                        <li><button className={styles.buttonNavigation} onClick={() => {setSelectedTable("orders")}}>Orders</button></li>
                     </ul>
                 </div>
                 <div className={styles.paymentLayout}>
                     <div className={styles.paymentHeader}>
-                        <h1 className={styles.sectionTitle}>{selectedSection}</h1>
+                        <h1 className={styles.sectionTitle}>{selectedTable}</h1>
+                        <div className={styles.tableHeaderActions}>
+                            <button className={styles.actionButton} onClick={() => {showNotification("Saving...");}}>
+                                <IoSaveOutline className={styles.iconClose} />
+                            </button>
+                            <button className={styles.actionButton} onClick={() => {fetchData(selectedTable); showNotification("Loading...");}}>
+                                <IoReload className={styles.iconClose} />
+                            </button>
+                        </div>
                     </div>
                     
-                    <input type='name' placeholder='Cardholder Name'></input>
-                    <input type='number' placeholder='Credit Card Number'></input>
-                    <input type='month' ></input>
-                    <input type='number' placeholder='CSV' maxLength={3}></input>
+                    <table className={styles.editableTable}>
+                        <thead>
+                            <tr>
+                                {tableColumnHeaders[selectedTable].map((title, index) => (
+                                    <th key={index}>{title}</th>
+                                ))}
+                            </tr>
+                            
+                        </thead>
+                        <tbody>
+                            {tableData[selectedTable] ? tableData[selectedTable].map((row) => (
+                                <tr key={row.user_id}>
+                                    {tableColumnHeaders[selectedTable].map((col, index) => (
+                                        <td key={index}>
+                                            <input 
+                                            type="text"
+                                            value={row[col.toLowerCase().replace(/\s+/g, "_")] || ""}/>
+                                        </td>
+                                    ))}
+                                </tr>
+                            )) : (<p>No data available</p>)}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
