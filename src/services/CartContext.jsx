43d7e523b4fd6812a83220afc4447c6e63e1e144
Chris <chrismc16@outlook.com>
2025-03-28 18:19:27 -0700

    - Updated CartContext.jsx; Fixed an issue that prevented the cart from updating when a user logged in.

--- src/services/CartContext.jsx
+++ src/services/CartContext.jsx
@@ -112,7 +112,7 @@ export const CartProvider = ({ children }) => {
     // --- Load Cart from LocalStorage on Mount ---
     useEffect(() => {
         if (isAuthenticated) loadRemoteCart();
-    }, []);
+    }, [isAuthenticated]);
 
     // --- Save Cart to LocalStorage on Change ---
     useEffect(() => {
